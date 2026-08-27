7baae8aeb04c7c60cf79084d9c0b45d0ca855632
Chiwon <71111212+Smarttiger2338@users.noreply.github.com>
2026-08-27 21:03:51 +0900

    Add friendly transfer usage example

---
+++ examples/friendly_transfer_example.py
@@ -0,0 +1,24 @@
+from transitguard.easy import assess_simple_transfer
+
+
+assessment = assess_simple_transfer(
+    requested_start="08:35",
+    origin_station="대구역앞",
+    transfer_station="중앙로역",
+    destination_station="동대구역건너",
+    first_route="101",
+    second_route="708",
+    first_departure="08:40",
+    transfer_arrival="08:55",
+    second_departure="09:04",
+    final_arrival="09:20",
+    next_vehicle_arrivals=["09:04", "09:12"],
+    walking_minutes=4,
+    minimum_buffer_minutes=3,
+)
+
+print(assessment.summary)
+print("추천:")
+for suggestion in assessment.suggestions:
+    print("-", suggestion)
+print("상세:", assessment.to_dict())
