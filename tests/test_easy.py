a6577c46977da566278a4eee5e6298a43bfd8977
Chiwon <71111212+Smarttiger2338@users.noreply.github.com>
2026-08-27 21:04:46 +0900

    Fix friendly assessment safe example timing

--- tests/test_easy.py
+++ tests/test_easy.py
@@ -61,9 +61,9 @@ def test_assess_simple_transfer_returns_korean_safe_summary():
         second_route="708",
         first_departure="08:40",
         transfer_arrival="08:55",
-        second_departure="09:04",
-        final_arrival="09:20",
-        next_vehicle_arrivals=["09:04", "09:12"],
+        second_departure="09:06",
+        final_arrival="09:22",
+        next_vehicle_arrivals=["09:06", "09:12"],
         walking_minutes=4,
         minimum_buffer_minutes=3,
     )
